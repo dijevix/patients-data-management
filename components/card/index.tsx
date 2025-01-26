dcfd2a48b79d1d878436275e9b6307d7c20b5dcc
Diego Vielma <dijevic.developer@gmail.com>
2025-01-26 20:22:16 -0300

    FEATURE 003 :add collapse/expand

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -7,9 +7,11 @@ import styles from "./styles.module.css";
 import defaultUser from "@/public/user_4302027.png";
 import { useState } from "react";
 import PencilIcon from "../icons/pencil";
+import UpArrowIcon from "../icons/upArrow";
 
 const UserCard = (user: IPatients) => {
   const [imageSrc, setImageSrc] = useState(user?.avatar || defaultUser.src);
+  const [showDescription, setShowDescription] = useState(false);
   return (
     <div className={styles.card_container}>
       <div className={styles.card_header}>
@@ -24,16 +26,28 @@ const UserCard = (user: IPatients) => {
 
         <h3 className={styles.card_title}>{user.name}</h3>
         <span className={styles.card_pencil}>
-        <PencilIcon/>
+          <PencilIcon />
         </span>
       </div>
       <div className={styles.card_date}>
         <span className={styles.card_date_title}>Created At</span>
         <span className={styles.card_date_date}>20-06-2024</span>
       </div>
-      {/* //! TODO : SHOW EXTRA INFORMATION */}
-      <p style={{display:'none'}}>{user.description}</p>
-      <p style={{display:'none'}}>{user.website}</p>
+      <div className={`${styles.card_expand} ${showDescription && styles.card_collapsed}`}   onClick={() => setShowDescription(!showDescription)}>
+        <UpArrowIcon />
+      </div>
+
+        <div >
+          <h3 className={styles.subtitle}>Description</h3>
+          <p className={`${styles.card_description} ${showDescription && styles.card_description_show}`}>{user.description}</p>
+          <h3 className={styles.subtitle}>Website</h3>
+
+          <div className={`${styles.card_description} ${showDescription && styles.card_description_show}`}>
+            <a href="user.website" target="blank">{user.website}</a>
+          </div>
+          
+        </div>
+      
     </div>
   );
 };
